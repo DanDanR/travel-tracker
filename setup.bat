4da04ccc6ab2e9fa0717cd36cedb3a47ddaf033f
DanDanR <80512734+DanDanR@users.noreply.github.com>
2026-09-10 20:01:24 +0200

    setup.bat fixed

--- setup.bat
+++ setup.bat
@@ -5,6 +5,11 @@ set "PROJDIR=%~dp0"
 if "%PROJDIR:~-1%"=="\" set "PROJDIR=%PROJDIR:~0,-1%"
 
 (
-echo "INTERPRETER_WIN","%PROJDIR%\venv\Scripts\python.exe"
-echo "PYTHONPATH","%PROJDIR%"
+    echo "INTERPRETER_WIN","%PROJDIR%\venv\Scripts\python.exe"
+    echo "PYTHONPATH","%PROJDIR%"
 ) > xlwings.conf
+
+(
+    echo RESTCOUNTRIES_API_KEY="your restcountries api key goes here"
+    echo GEONAMES_USERNAME="your geonames username goes here"
+) > credentials.txt
